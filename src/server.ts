26c3c090fd0f0180da78e846a237dcf32376739e
Bruno de Rezende <brunoderezende@hotmail.com>
2020-03-28 18:16:46 -0300

    Adds filter route

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -27,16 +27,29 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
+
+  app.get("/filteredimage", async (req, res) => {
+    //    1. validate the image_url query
+    const imageUrl = req.query.image_url;
+    // 2. call filterImageFromURL(image_url) to filter the image
+    filterImageFromURL(imageUrl).then( filteredPath => {
+      // 3. send the resulting file in the response
+      res.status(200).sendFile(filteredPath, err => {
+        // 4. deletes any files on the server on finish of the response
+        deleteLocalFiles([filteredPath]);
+        if(err) console.log(err);
+      });
+    }).catch( err => res.status(422).send(err));
+  });
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
+
 
   // Start the Server
   app.listen( port, () => {
